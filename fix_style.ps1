423b1d578fe64821ae900dfd7c416949c2bfd8c6
chanseo0916-bit <chanseo0916@gmail.com>
2026-02-24 20:55:09 +0900

    Delete fix_style.ps1

--- fix_style.ps1
+++
@@ -1,3 +0,0 @@
-$content = Get-Content "c:\Users\이찬서\OneDrive\바탕 화면\founderex_landing\style.css" -Encoding UTF8
-$newContent = $content[0..1521] + $content[1524..($content.Length-1)]
-$newContent | Set-Content "c:\Users\이찬서\OneDrive\바탕 화면\founderex_landing\style.css" -Encoding UTF8
